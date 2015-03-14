a750d9815e7ac5499cd1c948e067d35f5f1b18f3
daxlab <mandeep25894@gmail.com>
2015-03-15 04:38:52 +0530

    added About section, status bar

--- src/main_page.py
+++ src/main_page.py
@@ -9,7 +9,23 @@ class MyWindowClass(QtGui.QMainWindow, first_page):
         self.setupUi(self)
         self.push_supervised.clicked.connect(self.push_supervised_clicked)  # Bind the event handlers
         self.push_unsupervised.clicked.connect(self.push_unsupervised_clicked)  #   to the buttons
+
+        self.menuHelp.addAction('&About', self.about)  # add About to Help
+        self.statusBar().showMessage("Start Page", 10000)
         
+    def about(self):
+        QtGui.QMessageBox.about(self, "About",
+"""                                                Minor Project
+
+Copyright 2015 Arjun P.P. , Mandeep Singh
+
+This is minor project for our undergraduate course in Information Technology. The project demonstrates some Machine Learning algorithms.
+
+
+The project uses Qt framework along with PyQt 4 plugin to create the UI.
+"""
+)
+    
     def push_supervised_clicked(self):
         self 
         
