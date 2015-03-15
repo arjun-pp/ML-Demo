2b8b978af80ad79b8df79ed6c7723c0d157e0b70
jamesalcatraz <aarjun.pp@gmail.com>
2015-03-15 15:11:37 +0530

    Added about message

---
+++ src/constants.py
@@ -0,0 +1,10 @@
+#About Message for help
+
+ABOUT_MESSAGE = """                                                Minor Project
+
+Copyright 2015 Arjun P.P. , Mandeep Singh
+
+This is minor project for our undergraduate course in Information Technology. The project demonstrates some Machine Learning algorithms.
+
+The project uses Qt framework along with PyQt 4 plugin to create the UI.
+"""
