c0f9a50ea3b709c7a2abf03e1ec6b728dd66091e
jamesalcatraz <aarjun.pp@gmail.com>
2015-03-15 14:19:28 +0530

    Fixed path

--- src/main_page.py
+++ src/main_page.py
@@ -6,9 +6,12 @@ class MyWindowClass(QtGui.QMainWindow, first_page):
     
     def __init__(self, parent=None):
         QtGui.QMainWindow.__init__(self, parent)
+        
         self.setupUi(self)
         self.push_supervised.clicked.connect(self.push_supervised_clicked)  # Bind the event handlers
         self.push_unsupervised.clicked.connect(self.push_unsupervised_clicked)  #   to the buttons
+        
+        
         
     def push_supervised_clicked(self):
         self 
@@ -24,4 +27,5 @@ class MyWindowClass(QtGui.QMainWindow, first_page):
 app = QtGui.QApplication(sys.argv)
 myWindow = MyWindowClass(None)
 myWindow.show()
+
 app.exec_()
