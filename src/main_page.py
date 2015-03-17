8d541a2f9f28cbbc778478f14166b00915bc3c00
jamesalcatraz <aarjun.pp@gmail.com>
2015-03-18 02:20:13 +0530

    Redefined methods for buttons

--- src/main_page.py
+++ src/main_page.py
@@ -10,8 +10,10 @@ class MyWindowClass(QtGui.QMainWindow, first_page):
         QtGui.QMainWindow.__init__(self, parent)
         
         self.setupUi(self)
-        self.push_supervised.clicked.connect(self.push_supervised_clicked)  # Bind the event handlers
-        self.push_unsupervised.clicked.connect(self.push_unsupervised_clicked)  #   to the buttons
+        self.push_knn.clicked.connect(self.push_knn_clicked)  # Bind the event handlers
+        self.push_naive.clicked.connect(self.push_naive_clicked)  #   to the buttons
+        self.push_kmeans.clicked.connect(self.push_kmeans_clicked)
+        self.push_fcm.clicked.connect(self.push_fcm_clicked)
 
         self.menuHelp.addAction('&About', self.about)  # add About to Help
         self.statusBar().showMessage("Start Page")
@@ -21,11 +23,18 @@ class MyWindowClass(QtGui.QMainWindow, first_page):
     def about(self):
         QtGui.QMessageBox.about(self, "About",constants.ABOUT_MESSAGE)
     
-    def push_supervised_clicked(self):
+    def push_knn_clicked(self):
         self 
         
-    def push_unsupervised_clicked(self):
+    def push_naive_clicked(self):
         self 
+        
+    def push_kmeans_clicked(self):
+        self
+        
+        
+    def push_fcm_clicked(self):
+        self
         
 
 app = QtGui.QApplication(sys.argv)
